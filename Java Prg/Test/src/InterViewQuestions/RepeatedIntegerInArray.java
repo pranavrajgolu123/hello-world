c5cb7f938a5f516cb80288a22e7c4e63aa403e5c
pranavraj8521@gmail.com <pranavraj8521@gmail.com>
2019-02-07 20:33:48 +0530

    Modify RepeatedIntegerInAnArray

--- Java Prg/Test/src/InterViewQuestions/RepeatedIntegerInArray.java
+++ Java Prg/Test/src/InterViewQuestions/RepeatedIntegerInArray.java
@@ -6,28 +6,35 @@ import java.util.List;
 public class RepeatedIntegerInArray {
 
 	public static void main(String rf[]) {
-		int arr[]= {1,2,3,4,5,3,2,1};
+		int arr[]= {1,2,3,4,5,3,2,1,5,6,7};
 		int count=1;
-		ArrayList<Integer> list = new ArrayList<Integer>(arr.length);
+		int countarr[]=new int[arr.length];
+		int time[]=new int[arr.length];
+/*		ArrayList<Integer> list = new ArrayList<Integer>(arr.length);
 		for (int i : arr) {
 			list.add(Integer.valueOf(i));
 		}
-		for(int i=0;i<list.size();i++) {
+		boolean t=list.contains(1);
+		System.out.println(t);
+		int tt=list.get(4);
+		System.out.println(tt);*/
+		for(int y=0; y<arr.length; y++) {
+			time[y]=1;
+		}
+		for(int i=0;i<arr.length;i++) {
 
-			for(int j=i+1;j<list.size();j++) {
-				if(arr[i] == arr[j]) {
-					count=count+1;
-					list.remove(j);
+			for(int j=i;j<arr.length;j++) {
+				if(arr[i] == arr[j] && i != j) {
+					countarr[i]=arr[i];
+					time[i]++;
+					
 				}
 			}
-			if(count!=1) {
-				System.out.println(arr[i]+"= "+count);
-				count=1;
-				}
-			else {
-				System.out.println(arr[i]+"= "+count);
-				
-				}
 		}
+		
+		for(int t=0;t<countarr.length;t++) {
+			System.out.println("\t" + arr[t] + "\t" +  countarr[t] + "\t" + time[t]);
+		}
+
 	}
 }
