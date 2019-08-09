94d87aa95337a7b26fda8a9d0e6ff6495feac8a3
pranavrajgolu123 <pranavraj8521@gmail.com>
2019-08-10 00:07:25 +0530

    Android

---
+++ Android Project/Ddd/app/src/main/java/com/example/raj/ddd/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.raj.ddd;
+
+/**
+ * Created by RAJ on 3/24/2017.
+ */
+
+public class AppConfig {
+
+    public static String URL_LOGIN = "http://192.168.0.108/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.108/android_login_api/register.php";
+}
